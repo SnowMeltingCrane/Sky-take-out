8407a71fe51fc812dd525c400468c570749fdf47
SnowCrane <1277456806@qq.com>
2025-01-25 05:23:05 +0800

    店铺营业状态设置业务代码开发

--- sky-server/src/main/java/com/sky/controller/user/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -1,4 +1,4 @@
-package com.sky.controller.admin;
+package com.sky.controller.user;
 
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
@@ -8,29 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("adminShopController")
-@RequestMapping("/admin/shop")
+@RestController("userShopController")
+@RequestMapping("/user/shop")
 @Api(tags = "店铺相关接口")
 @Slf4j
 public class ShopController {
 
     public static final String KEY = "SHOP_STATUS";
 
-     @Autowired
-     private RedisTemplate redisTemplate;
-
-    /**
-     * 设置店铺的营业状态
-     * @param status 状态码
-     * @return 状态码
-     */
-     @PutMapping("/{status}")
-     @ApiOperation("设置店铺的营业状态")
-     public Result<String> setStatus(@PathVariable Integer status) {
-         log.info("设置店铺的营业状态:{}", status == 1 ? "营业中" : "打烊中");
-         redisTemplate.opsForValue().set(KEY, status);
-         return Result.success("");
-    }
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * 获取店铺的营业状态
